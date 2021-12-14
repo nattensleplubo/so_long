2c0a367fb2ae4fdccd95e5f17469b2e760a44b83
Nathan <nathan.gobert2002@gmail.com>
2021-12-14 16:22:41 +0100

    Added maplen to calculate the map's legnth

--- ft_checkmap.c
+++ ft_checkmap.c
@@ -6,75 +6,23 @@
 /*   By: ngobert <ngobert@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/09 10:15:16 by ngobert           #+#    #+#             */
-/*   Updated: 2021/12/09 14:43:50 by ngobert          ###   ########.fr       */
+/*   Updated: 2021/12/14 16:21:42 by ngobert          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_solong.h"
+#include "get_next_line.h"
 
-
-int	ft_checkmapsquare(char *map)
+int	ft_maplen(char *map)
 {
-	char	*str;
-	int		line1;
-	int		i;
-	int		len;
 	int		fd;
+	char	*line;
+	int		i;
 
 	fd = open(map, O_RDONLY);
-	str = get_next_line(fd);
-	line1 = ft_strlen(str);
-	i = 1;
-	while (str != NULL)
-	{
-		str = get_next_line(fd);
-		len = ft_strlen(str);
-		if (len != line1)
-		{
-			return (-1);
-			close(fd);
-		}
+	while (get_next_line(fd) != NULL)
 		i++;
-	}
-	close(fd);
 	return (i);
 }
 
-int	check_ns_borders(char *border)
-{
-	int	i;
 
-	i = 0;
-	while (border[i] != '\n')
-	{
-		if (border[i] != '1');
-			return (-1);
-		i++;
-	}
-}
-
-int	ft_checkmapclosed(char *map, int size)
-{
-	int		fd;
-	char	*line1;
-	int		len;
-	int		i;
-
-	i = 0;
-	fd = open(map, O_RDONLY);
-	line1 = get_next_line(fd);
-	len = ft_strlen(line1);
-	while (str != NULL && i < size)
-	{
-		if ()
-	}
-}
-
-
-// int	ft_checkglobal(char *map)
-// {
-// 	if (ft_checkmapsquare(map) > 0 && ft_checkmapclosed(map, ft_checkmapsquare) > 0)
-// 	{
-		
-// 	}
-// }
--- ft_solong.h
+++ ft_solong.h
@@ -6,11 +6,11 @@
 /*   By: ngobert <ngobert@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/08 16:26:01 by ngobert           #+#    #+#             */
-/*   Updated: 2021/12/09 12:16:26 by ngobert          ###   ########.fr       */
+/*   Updated: 2021/12/14 13:38:23 by ngobert          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-ifndef FT_SOLONG_H
+#ifndef FT_SOLONG_H
 # define FT_SOLONG_H
 
 # include <stdio.h>
