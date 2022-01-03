dfabefd2c92a268610f982a37884795a7baddb0b
Nathan <nathan.gobert2002@gmail.com>
2022-01-03 12:49:04 +0100

    Added some structs

--- ft_solong.h
+++ ft_solong.h
@@ -6,7 +6,7 @@
 /*   By: ngobert <ngobert@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/08 16:26:01 by ngobert           #+#    #+#             */
-/*   Updated: 2022/01/02 23:42:55 by ngobert          ###   ########.fr       */
+/*   Updated: 2022/01/03 12:48:21 by ngobert          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,6 +24,58 @@
 # define COIN "./assets/sprites/coin.xpm"
 # define EXIT "./assets/sprites/exit.xpm"
 # define PLAYER "./assets/sprites/player.xpm"
+
+typedef enum e_bool {TRUE = 1, FALSE = 0}	t_bool;
+
+typedef	struct	s_vars
+{
+	void	*mlx;
+	void	*win;
+}	t_vars;
+
+typedef struct	s_map
+{
+	char	**map;
+	int		fd;
+	int		width;
+	int		height;
+	int		col;
+	int		row;
+}	t_map;
+
+typedef struct	s_player
+{
+	int	player_x;
+	int	player_y;
+	int	player_moves;
+}	t_player;
+
+typedef struct	s_coin
+{
+	int	coin_total;
+	int	coin_taken;
+}	t_coin;
+
+typedef struct	s_sprite
+{
+	void	*wall;
+	void	*ground;
+	void	*coin;
+	void	*exit;
+	void	*player;
+	int		img_width;
+	int		img_height;
+}	t_sprite;
+
+typedef struct	s_module
+{
+	t_vars		*vars;
+	t_map		*map;
+	t_player	*player;
+	t_coin		*coin;
+	t_sprite	*sprite;
+	t_bool		end_game;
+}	t_module;
 
 char	*get_next_line(int fd);
 int		ft_mapcheck(char *map, int len);
