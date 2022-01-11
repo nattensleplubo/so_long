0a7c011930b72c6486d565ffd66fb4674f253bd6
Nathan GOBERT <ngobert@e2r2p19.clusters.42paris.fr>
2022-01-11 11:51:59 +0100

    jai pas fait lexam en fin de compte

--- so_long.c
+++ so_long.c
@@ -6,7 +6,21 @@
 /*   By: ngobert <ngobert@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/11 11:36:53 by ngobert           #+#    #+#             */
-/*   Updated: 2022/01/11 11:36:54 by ngobert          ###   ########.fr       */
+/*   Updated: 2022/01/11 11:47:33 by ngobert          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "ft_solong.h"
+
+int	main (int argc, char **argv)
+{
+	t_module	*module;
+
+	if (argc == 2)
+	{
+		module = malloc(1 * sizeof(t_module));
+		if (module == NULL)
+			return (-1);
+		if (ft_mapcheck(argv[1]) == 1)
+	}
+}
